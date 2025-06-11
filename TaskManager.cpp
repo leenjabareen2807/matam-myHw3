7bb956b5e5dab967ade2c76c66a73f5b1263314b
tahleel2812 <tahalilm@campus.technion.ac.il>
2025-06-11 20:13:16 +0300

    add assignTask function

--- TaskManager.cpp
+++ TaskManager.cpp
@@ -1,3 +1,32 @@
 //
 // Created by leenn on 6/11/2025.
 //
+#include "TaskManager.h"
+
+TaskManager::TaskManager() {
+    TaskId=0;
+    personsNum=0;
+}
+void TaskManager::assignTask(const string& name, const Task& task) {
+    int personIndex = -1;
+    for (int i = 0; i < personsNum && i < MAX_PERSONS; i++) {
+        if (persons[i].getName() == name) {
+            personIndex = i;
+            break;
+        }
+    }
+    if (personIndex == -1) {
+        if (personsNum >= MAX_PERSONS) {
+            throw std::runtime_error("TaskManager is FULL");
+        }
+        Person newPerson(name);
+        persons[personsNum] = newPerson;
+        personIndex = personsNum;
+        personsNum++;
+    }
+    Task newTask(task);
+    newTask.setId(TaskId++);
+    persons[personIndex].assignTask(newTask);
+}
+
+
